216810e2d268d87d33eebac3e00576f1200da570
Navin3d <smnavin65@mail.com>
2023-06-02 12:51:36 +0530

    Done development of version 0.0.1

---
+++ index.js
@@ -0,0 +1,181 @@
+const fs = require("fs");
+const process = require("process");
+
+class Logging {
+
+    constructor() {
+        this.timeStamp = null;
+        const packageJsonPath = './package.json';
+        try {
+            const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
+            this.projectName = packageJson.name;
+        } catch (error) {
+            this.projectName = "UNKNOWN";
+        }
+    }
+
+    log(message) {
+        this.dateObj = new Date();
+        const timeStamp = `${this.dateObj.getFullYear()}-${this.dateObj.getMonth()}-${this.dateObj.getDate()} ${this.dateObj.getHours()}:${this.dateObj.getMinutes()}:${this.dateObj.getSeconds()}:${this.dateObj.getMilliseconds()}`;
+        const messageType = `[LOG]`;
+        const processId = process.pid;
+        let filePath = "FileName";
+        var _pst = Error.prepareStackTrace
+        Error.prepareStackTrace = function (err, stack) { return stack; };
+        try {
+            var err = new Error();
+            var callerfile;
+            var currentfile;
+            currentfile = err.stack.shift().getFileName();
+            while (err.stack.length) {
+                callerfile = err.stack.shift().getFileName();
+                if (currentfile !== callerfile) {
+                    filePath = callerfile.split("/");
+                    break;
+                }
+            }
+        } catch (err) { }
+        Error.prepareStackTrace = _pst;
+        const fileName = `${filePath[filePath.length - 2]}/${filePath[filePath.length - 1]}`;
+        const out = `${timeStamp}  ${messageType} ${processId} ===> [${this.projectName}] ${fileName} : ${message}`;
+        console.log(out);
+    }
+
+    info(message) {
+        this.dateObj = new Date();
+        const timeStamp = `${this.dateObj.getFullYear()}-${this.dateObj.getMonth()}-${this.dateObj.getDate()} ${this.dateObj.getHours()}:${this.dateObj.getMinutes()}:${this.dateObj.getSeconds()}:${this.dateObj.getMilliseconds()}`;
+        const messageType = `[INFO]`;
+        const processId = process.pid;
+        let filePath = "FileName";
+        var _pst = Error.prepareStackTrace
+        Error.prepareStackTrace = function (err, stack) { return stack; };
+        try {
+            var err = new Error();
+            var callerfile;
+            var currentfile;
+            currentfile = err.stack.shift().getFileName();
+            while (err.stack.length) {
+                callerfile = err.stack.shift().getFileName();
+                if (currentfile !== callerfile) {
+                    filePath = callerfile.split("/");
+                    break;
+                }
+            }
+        } catch (err) { }
+        Error.prepareStackTrace = _pst;
+        const fileName = `${filePath[filePath.length - 2]}/${filePath[filePath.length - 1]}`;
+        const out = `${timeStamp}  ${messageType} ${processId} ===> [${this.projectName}] ${fileName} : ${message}`;
+        console.info(out);
+    }
+
+    warn(message) {
+        this.dateObj = new Date();
+        const timeStamp = `${this.dateObj.getFullYear()}-${this.dateObj.getMonth()}-${this.dateObj.getDate()} ${this.dateObj.getHours()}:${this.dateObj.getMinutes()}:${this.dateObj.getSeconds()}:${this.dateObj.getMilliseconds()}`;
+        const messageType = `[WARN]`;
+        const processId = process.pid;
+        let filePath = "FileName";
+        var _pst = Error.prepareStackTrace
+        Error.prepareStackTrace = function (err, stack) { return stack; };
+        try {
+            var err = new Error();
+            var callerfile;
+            var currentfile;
+            currentfile = err.stack.shift().getFileName();
+            while (err.stack.length) {
+                callerfile = err.stack.shift().getFileName();
+                if (currentfile !== callerfile) {
+                    filePath = callerfile.split("/");
+                    break;
+                }
+            }
+        } catch (err) { }
+        Error.prepareStackTrace = _pst;
+        const fileName = `${filePath[filePath.length - 2]}/${filePath[filePath.length - 1]}`;
+        const out = `${timeStamp}  ${messageType} ${processId} ===> [${this.projectName}] ${fileName} : ${message}`;
+        console.warn(out);
+    }
+
+    trace(message) {
+        this.dateObj = new Date();
+        const timeStamp = `${this.dateObj.getFullYear()}-${this.dateObj.getMonth()}-${this.dateObj.getDate()} ${this.dateObj.getHours()}:${this.dateObj.getMinutes()}:${this.dateObj.getSeconds()}:${this.dateObj.getMilliseconds()}`;
+        const messageType = `[TRACE]`;
+        const processId = process.pid;
+        let filePath = "FileName";
+        var _pst = Error.prepareStackTrace
+        Error.prepareStackTrace = function (err, stack) { return stack; };
+        try {
+            var err = new Error();
+            var callerfile;
+            var currentfile;
+            currentfile = err.stack.shift().getFileName();
+            while (err.stack.length) {
+                callerfile = err.stack.shift().getFileName();
+                if (currentfile !== callerfile) {
+                    filePath = callerfile.split("/");
+                    break;
+                }
+            }
+        } catch (err) { }
+        Error.prepareStackTrace = _pst;
+        const fileName = `${filePath[filePath.length - 2]}/${filePath[filePath.length - 1]}`;
+        const out = `${timeStamp}  ${messageType} ${processId} ===> [${this.projectName}] ${fileName} : ${message}`;
+        console.trace(out);
+    }
+
+    debug(message) {
+        this.dateObj = new Date();
+        const timeStamp = `${this.dateObj.getFullYear()}-${this.dateObj.getMonth()}-${this.dateObj.getDate()} ${this.dateObj.getHours()}:${this.dateObj.getMinutes()}:${this.dateObj.getSeconds()}:${this.dateObj.getMilliseconds()}`;
+        const messageType = `[DEBUG]`;
+        const processId = process.pid;
+        let filePath = "FileName";
+        var _pst = Error.prepareStackTrace
+        Error.prepareStackTrace = function (err, stack) { return stack; };
+        try {
+            var err = new Error();
+            var callerfile;
+            var currentfile;
+            currentfile = err.stack.shift().getFileName();
+            while (err.stack.length) {
+                callerfile = err.stack.shift().getFileName();
+                if (currentfile !== callerfile) {
+                    filePath = callerfile.split("/");
+                    break;
+                }
+            }
+        } catch (err) { }
+        Error.prepareStackTrace = _pst;
+        const fileName = `${filePath[filePath.length - 2]}/${filePath[filePath.length - 1]}`;
+        const out = `${timeStamp}  ${messageType} ${processId} ===> [${this.projectName}] ${fileName} : ${message}`;
+        console.debug(out);
+    }
+
+    error(message) {
+        this.dateObj = new Date();
+        const timeStamp = `${this.dateObj.getFullYear()}-${this.dateObj.getMonth()}-${this.dateObj.getDate()} ${this.dateObj.getHours()}:${this.dateObj.getMinutes()}:${this.dateObj.getSeconds()}:${this.dateObj.getMilliseconds()}`;
+        const messageType = `[ERROR]`;
+        const processId = process.pid;
+        let filePath = "FileName";
+        var _pst = Error.prepareStackTrace
+        Error.prepareStackTrace = function (err, stack) { return stack; };
+        try {
+            var err = new Error();
+            var callerfile;
+            var currentfile;
+            currentfile = err.stack.shift().getFileName();
+            while (err.stack.length) {
+                callerfile = err.stack.shift().getFileName();
+                if (currentfile !== callerfile) {
+                    filePath = callerfile.split("/");
+                    break;
+                }
+            }
+        } catch (err) { }
+        Error.prepareStackTrace = _pst;
+        const fileName = `${filePath[filePath.length - 2]}/${filePath[filePath.length - 1]}`;
+        const out = `${timeStamp}  ${messageType} ${processId} ===> [${this.projectName}] ${fileName} : ${message}`;
+        console.error(out);
+    }
+
+}
+
+module.exports = Logging;
---
+++ test.js
@@ -0,0 +1,10 @@
+const Logging = require("./index");
+
+const log = new Logging();
+
+log.log("Hello world!");
+log.info("Hello world!");
+log.warn("Hello world!");
+log.debug("Hello world!");
+log.trace("Hello world!");
+log.error("Hello world!");
